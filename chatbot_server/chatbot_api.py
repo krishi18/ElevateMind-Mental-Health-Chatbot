06d7080b5456f6c56420e365415574f09c04533f
TASDevelopers <thearcsoldevelopers@gmail.com>
2024-12-29 20:18:08 +0530

    added port

--- chatbot_server/chatbot_api.py
+++ chatbot_server/chatbot_api.py
@@ -226,4 +226,5 @@ def index():
     return "ElevateMind's Mental Health Chatbot API"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
